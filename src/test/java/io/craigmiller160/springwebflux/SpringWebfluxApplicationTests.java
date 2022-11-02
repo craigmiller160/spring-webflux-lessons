31989031401fd4ce0bce2980c4a445adc7883e80
Craig Miller <craigmiller160@gmail.com>
2022-11-02 15:33:15 -0400

    Initial commit

---
+++ src/test/java/io/craigmiller160/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.craigmiller160.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
