a9a0fe756a1d4773410bfafd4590a08ebe9b64f6
Craig Miller <craigmiller160@gmail.com>
2022-11-03 11:07:54 -0400

    Cleanup

--- src/test/java/io/craigmiller160/springwebflux/controller/FluxAndMonoControllerTest.java
+++ src/test/java/io/craigmiller160/springwebflux/controller/FluxAndMonoControllerTest.java
@@ -6,17 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
